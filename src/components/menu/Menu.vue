48cac037e9ce96e1c900a5b32d23355d24c29021
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-05-28 14:57:52 +0400

    Star Wars

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,11 +1,11 @@
 <template>
   <div :class="`absolute top-[5%] right-[2%] w-[100px] h-[100px]`">
     <ul>
-      <RouterLink to="/people">
-        <li class="hover:font-bold">Hunter's</li>
-      </RouterLink>
       <RouterLink to="/">
         <li class="hover:font-bold">Home</li>
+      </RouterLink>
+      <RouterLink to="/people">
+        <li class="hover:font-bold">Hunter's</li>
       </RouterLink>
       <RouterLink to="/starship">
         <li class="hover:font-bold">Star Ships</li>
